670c2dee43d00408a034bf3dae69990afb18cfa3
jingsu <victoriasu1994@gmail.com>
2019-01-03 15:51:01 -0500

    Add criteria info

--- src/pages/patientView/matchMiner/MatchMiner.tsx
+++ src/pages/patientView/matchMiner/MatchMiner.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { If, Then, Else } from 'react-if';
 import * as _ from 'lodash';
 import {observer} from "mobx-react";
-import { INctTrial, ITrial, ITrialMatch } from "../../../shared/model/MatchMiner";
+import { IEligibility, INctTrial, ITrial, ITrialMatch } from "../../../shared/model/MatchMiner";
 import styles from './style/MatchMiner.module.scss';
 
 export type IMatchMinerProps = {
@@ -16,15 +16,14 @@ export default class MatchMiner extends React.Component<IMatchMinerProps, {detai
 
     constructor(props: IMatchMinerProps){
         super();
-        this.state = { detailedTrialMatches: this.buildDetaildTrialMatches(props.trialMatches, props.trials, props.nctTrials)};
+        this.state = { detailedTrialMatches: this.buildDetailedTrialMatches(props.trialMatches, props.trials, props.nctTrials)};
     }
 
     shouldComponentUpdate(nextProps: IMatchMinerProps){
         return nextProps === this.props;
     }
 
-    buildDetaildTrialMatches(matches: Array<ITrialMatch>, trials: Array<ITrial>, nctTrials: Array<INctTrial>) {
-
+    buildDetailedTrialMatches(matches: Array<ITrialMatch>, trials: Array<ITrial>, nctTrials: Array<INctTrial>) {
         let groupedMatches = _.groupBy(matches, 'nctId');
         let matchedTrials: Array<any> = [];
         const trialIds = Object.keys(groupedMatches);
@@ -42,6 +41,36 @@ export default class MatchMiner extends React.Component<IMatchMinerProps, {detai
                 if (key === trial['nctId']) {
                     matchedTrial['diseases'] = trial['diseases'];
                     matchedTrial['interventions'] = trial['interventions'];
+                    let eligibility: IEligibility = {
+                        clinical:{
+                            age: '',
+                            sex: ''
+                        },
+                        genomic: {
+                            inclusion: [],
+                            exclusion: []
+                        }
+                    };
+                    const structuredEligibility = trial['eligibility']['structured'];
+                    const unstructuredEligibility = trial['eligibility']['unstructured'];
+                    if (structuredEligibility['max_age_number'] > 99) {
+                        eligibility.clinical.age = structuredEligibility['min_age'] + ' and older'
+                    } else {
+                        eligibility.clinical.age = structuredEligibility['min_age'] + ' ~ ' + structuredEligibility['max_age']
+                    }
+                    if (structuredEligibility['gender'] === 'BOTH') {
+                        eligibility.clinical.sex = 'All';
+                    } else {
+                        eligibility.clinical.sex = structuredEligibility['gender'];
+                    }
+                    _.forEach(unstructuredEligibility, function(ele) {
+                        if (ele['inclusion_indicator']) {
+                            eligibility.genomic.inclusion.push(ele);
+                        } else {
+                            eligibility.genomic.exclusion.push(ele);
+                        }
+                    });
+                    matchedTrial['eligibility'] = eligibility;
                     return true;
                 }
             });
@@ -56,50 +85,13 @@ export default class MatchMiner extends React.Component<IMatchMinerProps, {detai
             return (
                 <tr>
                     <td>
-                        <div className="row">
-                            <div className="col-md-5">
-                                {trial.matches.map(function(match: any) {
-                                    return (
-                                        <If condition={match.trueHugoSymbol || match.trueProteinChange}>
-                                            <span>{match.trueHugoSymbol + ' ' + match.trueProteinChange}</span>
-                                            <br/>
-                                        </If>
-                                    )}
-                                )}
-                            </div>
-                            <div className="col-md-1">
-                                <i className="fa fa-arrow-right" aria-hidden="true"></i>
-                            </div>
-                            <div className="col-md-6">
-                                {trial.matches.map(function(match: any) {
-                                    return (
-                                        <div>
-                                            <If condition={match.genomicAlteration.includes('!')}>
-                                                <Then>
-                                                    <span><b>No </b>{match.genomicAlteration.replace(/!/g, '',)}</span>
-                                                </Then>
-                                                <Else>
-                                                    <span>{match.genomicAlteration}</span>
-                                                </Else>
-                                            </If>
-                                            <br/>
-                                        </div>
-                                    )}
-                                )}
-                            </div>
-                        </div>
-                    </td>
-                    <td>
-                        <span>{trial.status}</span>
-                    </td>
-                    <td>
                         <span><a href={"https://clinicaltrials.gov/ct2/show/" + trial.nctId}>{trial.nctId}</a></span>
                     </td>
                     <td>
                         <span>{trial.shortTitle}</span>
                     </td>
                     <td>
-                        <ul>
+                        <ul className={styles.diseasesUl}>
                             {trial.diseases.map(function(disease: any) {
                                 return (
                                     <li>{disease}</li>
@@ -108,13 +100,63 @@ export default class MatchMiner extends React.Component<IMatchMinerProps, {detai
                         </ul>
                     </td>
                     <td>
-                        <ul>
+                        <ul className={styles.diseasesUl}>
                             {trial.interventions.map(function(intervention: any) {
                                 return (
                                     <li>{intervention}</li>
                                 )}
                             )}
                         </ul>
+                    </td>
+                    <td>
+                        <div>
+                            <span>Age:&nbsp;</span><span>{trial.eligibility.clinical.age}</span>
+                            <br/>
+                            <span>Sex:&nbsp;</span><span>{trial.eligibility.clinical.sex}</span>
+                            <br/>
+                            <div>
+                                <If condition={trial.eligibility.genomic.inclusion.length > 0}>
+                                    <button className={"btn btn-xs " + styles.criteriaButton + " " + styles.orangeButton}>
+                                        {trial.eligibility.genomic.inclusion.length} inclusion criteria
+                                    </button>
+                                </If>
+                                <If condition={trial.eligibility.genomic.exclusion.length > 0}>
+                                    <button className={"btn btn-xs " + styles.criteriaButton + " " + styles.greenButton}>
+                                        {trial.eligibility.genomic.exclusion.length} exclusion criteria
+                                    </button>
+                                </If>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        {trial.matches.map(function(match: any) {
+                            return (
+                                <div>
+                                    <If condition={match.genomicAlteration.includes('!')}>
+                                        <Then>
+                                            <span><b>No </b>{match.genomicAlteration.replace(/!/g, '',)}</span>
+                                        </Then>
+                                        <Else>
+                                            <span>{match.genomicAlteration}</span>
+                                        </Else>
+                                    </If>
+                                    <br/>
+                                </div>
+                            )}
+                        )}
+                        <span className={styles.fontItalic}>(Matched to patient with&nbsp;
+                            {trial.matches.map(function(match: any) {
+                                return (
+                                    <If condition={match.trueHugoSymbol || match.trueProteinChange}>
+                                        <span>{match.trueHugoSymbol + ' ' + match.trueProteinChange}</span>
+                                        <br/>
+                                    </If>
+                                )}
+                            )})
+                        </span>
+                    </td>
+                    <td>
+                        <span>{trial.status}</span>
                     </td>
                 </tr>
             )});
@@ -123,12 +165,13 @@ export default class MatchMiner extends React.Component<IMatchMinerProps, {detai
                 <table className="table table-striped">
                     <thead>
                     <tr>
-                        <th>Genomic Match</th>
-                        <th>Status</th>
                         <th>NCT ID</th>
                         <th className={styles.thTitle}>Title</th>
-                        <th>Disease</th>
+                        <th>Diseases</th>
                         <th>Interventions</th>
+                        <th>Criteria</th>
+                        <th>Genomic Match</th>
+                        <th>Status</th>
                     </tr>
                     </thead>
                     <tbody>
